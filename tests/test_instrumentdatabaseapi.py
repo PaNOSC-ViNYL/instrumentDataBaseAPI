81d1afc9fe23fc9ad84c558fdf67eac56dff3f83
Shervin Nourbakhsh <shervin.nourbakhsh@ill.fr>
2022-02-04 11:12:31 +0100

    first prototype of implementation

--- tests/test_instrumentdatabaseapi.py
+++ tests/test_instrumentdatabaseapi.py
@@ -6,7 +6,7 @@ import pytest
 
 from click.testing import CliRunner
 
-from instrumentdatabaseapi import instrumentdatabaseapi
+from instrumentdatabaseapi import instrumentdatabaseapi as API
 from instrumentdatabaseapi import cli
 
 
@@ -31,7 +31,31 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert 'instrumentdatabaseapi.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "instrumentdatabaseapi.cli.main" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help  Show this message and exit." in help_result.output
+
+
+def test_design_defaults():
+    """Test for API design"""
+
+    # init the local repo, making a git clone
+    API.init()
+    API.ls_instruments()  # print the list of instruments available
+    instrument_name = "ILL/D22"
+    API.ls_versions(instrument_name)
+
+    myinstrument = API.load_instrument("ILL/D22", "HEAD")
+    myinstrument.print_parameters()
+
+    myinstrument["wavelength"] = pint.Unit("5", "angstrom")
+
+    mysample = ""  # sampleAPI.load_sample()
+    # sampleAPI.ls_samples()
+
+    myinstrument.set_sample()  # ?
+    # or
+    API.set_sample(myinstrument, mysample)  # ?
+
+    mydate = myinstrument.run()
